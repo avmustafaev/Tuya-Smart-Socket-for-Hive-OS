c172770a713395b16dceee5e3a4b100df72adc70
avmustafaev <alexander.mustafaev@gmail.com>
2022-07-13 17:26:50 +0500

    bug

--- modules/notifiyer.py
+++ modules/notifiyer.py
@@ -2,5 +2,5 @@ from modules.connect_sql import sql_zapros as sqz
 
 
 def add_notify(rig_id, notification_type):
-    sql_string = 'INSERT OR IGNORE INTO farms_id VALUES (?,?)'
+    sql_string = 'INSERT OR IGNORE INTO notify VALUES (?,?)'
     sqz(sql_string, (rig_id, notification_type))
