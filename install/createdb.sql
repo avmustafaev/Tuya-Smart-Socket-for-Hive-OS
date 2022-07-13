69ee964285b82215180b7e35b127e74f4eeaac4e
avmustafaev <alexander.mustafaev@gmail.com>
2022-07-13 17:09:37 +0500

    sql

--- install/createdb.sql
+++ install/createdb.sql
@@ -28,9 +28,29 @@ CREATE TABLE powerman (
 );
 
 CREATE TABLE notify (
-    text STRING
+    rig_id STRING PRIMARY KEY,
+    notify STRING
 );
 
-
+CREATE TABLE comparison (
+    status_id   STRING PRIMARY KEY ON CONFLICT ABORT,
+    status_text STRING
+);
 
 INSERT INTO pref (name, value) VALUES ('pause', 'unpause');
+
+INSERT INTO comparison (status_id, status_text) VALUES ('self_heal', '🌱 Риг выздоровел самостоятельно');
+INSERT INTO comparison (status_id, status_text) VALUES ('socket_healed', '☘️ Розеточка помогла! перезагрузился!');
+INSERT INTO comparison (status_id, status_text) VALUES ('heal_from_emergency', '🍀 Риг восстановлен из аварийных');
+INSERT INTO comparison (status_id, status_text) VALUES ('silent', '🤐 Риг молчит, подождём, может обновляется или перезагружается');
+INSERT INTO comparison (status_id, status_text) VALUES ('no_socket', '🚫 Нет розетки! Сразу перевожу в аварийный статус');
+INSERT INTO comparison (status_id, status_text) VALUES ('too_long_silent_reboot', '♻️ Риг долго молчит — перезагружаем...');
+INSERT INTO comparison (status_id, status_text) VALUES ('has_problem_reboot', '🖲 Есть проблемы — перезагружаем...');
+INSERT INTO comparison (status_id, status_text) VALUES ('is_emergency', '🆘️ Авария!!! Риг не перезагрузился, отключаю питание, приезжайте разбирайтесь!');
+INSERT INTO comparison (status_id, status_text) VALUES ('heal_try', '🐣 Попытаюсь восстановить из аварийных');
+INSERT INTO comparison (status_id, status_text) VALUES ('rig_ignored', '🛠 Риг на обслуживании игнорирую’');
+INSERT INTO comparison (status_id, status_text) VALUES ('no_watchdog', '🪱 настройте watchdog на риге');
+INSERT INTO comparison (status_id, status_text) VALUES ('missed_unit', '🫥 Карта отсутствует');
+INSERT INTO comparison (status_id, status_text) VALUES ('no_hashrate', '💤 Нет хешрейта на риге');
+INSERT INTO comparison (status_id, status_text) VALUES ('missed_temp', '🌡 Нет данных температуры на карте');
+INSERT INTO comparison (status_id, status_text) VALUES ('missed_hashrate', '😴 Нет хешрейта на карте');
