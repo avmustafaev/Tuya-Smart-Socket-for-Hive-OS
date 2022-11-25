5afa617b3629d699cb4e43cc57c26096d25fbbfd
avmustafaev <alexander.mustafaev@gmail.com>
2022-11-25 23:31:43 +0500

    kas

--- modules/get_exbitron.py
+++ modules/get_exbitron.py
@@ -6,4 +6,5 @@ def get_exbitron():
     client = modules.exbitron.Client(access_key = exb_access_key, secret_key = exb_secret_key)
     rxd = round(float(client.get("/api/v2/peatio/account/balances/rxd")['balance']),2)
     usdt = round(float(client.get("/api/v2/peatio/account/balances/usdt")['balance']),2)
-    return f'☢️ Баланс RXD на бирже: {rxd}\n💰 Баланс USDT: {usdt}\n\n🌟\n\n\n'
+    kas = round(float(client.get("/api/v2/peatio/account/balances/kas")['balance']),2)
+    return f'☢️ Баланс RXD на бирже: {rxd}\n💰 Баланс USDT: {usdt}\n🅚 Баланс KAS: {kas}\n\n🌟\n\n\n'
