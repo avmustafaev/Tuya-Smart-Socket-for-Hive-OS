59453faf5b604b30b54d6d1c63d41552a3835e71
avmustafaev <alexander.mustafaev@gmail.com>
2022-11-25 23:34:38 +0500

    hour fix

--- modules/get_minute.py
+++ modules/get_minute.py
@@ -5,8 +5,8 @@ def start_hour():
     now = datetime.now()
     current_minute = int(now.strftime("%M"))
     current_hour = int(now.strftime("%H"))
-    hour_in = current_hour in {8, 12, 15, 17, 20}
-    minute_in = current_minute >= 0 and current_minute <11
+    hour_in = current_hour in {7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23}
+    minute_in = current_minute >= 0 and current_minute <10
     responce = minute_in and hour_in
     if responce:
         print("Отчёт ключевого часа")
