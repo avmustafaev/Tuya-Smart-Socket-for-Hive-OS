e4c5ea475507c4d789baf338228a8ab587a9acfb
avmustafaev <alexander.mustafaev@gmail.com>
2022-11-25 08:23:27 +0500

    fix space

--- modules/get_exbitron.py
+++ modules/get_exbitron.py
@@ -6,4 +6,4 @@ def get_exbitron():
     client = modules.exbitron.Client(access_key = exb_access_key, secret_key = exb_secret_key)
     rxd = round(float(client.get("/api/v2/peatio/account/balances/rxd")['balance']),2)
     usdt = round(float(client.get("/api/v2/peatio/account/balances/usdt")['balance']),2)
-    return f'☢️  Баланс RXD на бирже: {rxd}\n💰 Баланс USDT: {usdt}\n\n🌟\n\n\n'
+    return f'☢️ Баланс RXD на бирже: {rxd}\n💰 Баланс USDT: {usdt}\n\n🌟\n\n\n'
