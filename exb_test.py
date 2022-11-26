5ede51debfbf9d467e5c8e6eef3713faadb1d2b6
avmustafaev <alexander.mustafaev@gmail.com>
2022-11-26 11:20:01 +0500

    f

--- exb_test.py
+++ exb_test.py
@@ -1,13 +1,18 @@
 import modules.exbitron as exb
 from  modules.settings import exb_access_key, exb_secret_key
 
-client = exb.Client(access_key = exb_access_key, secret_key = exb_secret_key)
-rxd = client.get("/api/v2/peatio/account/balances/rxd")
-print(client.check_auth())
-print(rxd)
+def get_exbitron():
+    client = exb.Client(access_key = exb_access_key, secret_key = exb_secret_key)
+    rxd = client.get("/api/v2/peatio/account/balances/rxd")
+    usdt = client.get("/api/v2/peatio/account/balances/usdt")
+    kas = client.get("/api/v2/peatio/account/balances/kas")
+    rxd_balance = round(float(rxd['balance']),2)
+    rxd_locked = round(float(rxd['locked']),2)
+    usdt_locked = round(float(usdt['locked']),2)
+    kas_locked = round(float(kas['locked']),2)
+    usdt_balance = round(float(usdt['balance']),2)
+    kas_balance = round(float(kas['balance']),2)
+    return f'💰💰💰\nUSDT\nbalance: {usdt_balance}\nlocked: {usdt_locked}\n\nRXD\nbalance: {rxd_balance}\nlocked: {rxd_locked}\n\nKAS\nbalance {kas_balance}\nlocked: {kas_locked}\n💰💰💰\n'
 
-
-#rxd = usdt = round(float(client.get("/api/v2/peatio/account/balances/rxd")['balance']),2)
-#usdt = round(float(client.get("/api/v2/peatio/account/balances/usdt")['balance']),2)
-#kas = round(float(client.get("/api/v2/peatio/account/balances/kas")['balance']),2)
+print(get_exbitron())
 
--- modules/get_exbitron.py
+++ modules/get_exbitron.py
@@ -2,8 +2,14 @@ import modules.exbitron
 from modules.settings import exb_access_key, exb_secret_key
 
 def get_exbitron():
-    client = modules.exbitron.Client(access_key = exb_access_key, secret_key = exb_secret_key)
-    rxd = usdt = round(float(client.get("/api/v2/peatio/account/balances/rxd")['balance']),2)
-    usdt = round(float(client.get("/api/v2/peatio/account/balances/usdt")['balance']),2)
-    kas = round(float(client.get("/api/v2/peatio/account/balances/kas")['balance']),2)
-    return f'☢️ Баланс RXD на бирже: {rxd}\n💰 Баланс USDT: {usdt}\n🅚 Баланс KAS: {kas}\n\n🌟\n\n\n'
+    client = exb.Client(access_key = exb_access_key, secret_key = exb_secret_key)
+    rxd = client.get("/api/v2/peatio/account/balances/rxd")
+    usdt = client.get("/api/v2/peatio/account/balances/usdt")
+    kas = client.get("/api/v2/peatio/account/balances/kas")
+    rxd_balance = round(float(rxd['balance']),2)
+    rxd_locked = round(float(rxd['locked']),2)
+    usdt_locked = round(float(usdt['locked']),2)
+    kas_locked = round(float(kas['locked']),2)
+    usdt_balance = round(float(usdt['balance']),2)
+    kas_balance = round(float(kas['balance']),2)
+    return f'💰💰💰\nUSDT\nbalance: {usdt_balance}\nlocked: {usdt_locked}\n\nRXD\nbalance: {rxd_balance}\nlocked: {rxd_locked}\n\nKAS\nbalance {kas_balance}\nlocked: {kas_locked}\n💰💰💰\n'
