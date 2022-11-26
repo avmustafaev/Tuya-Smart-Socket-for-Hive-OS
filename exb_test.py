a9861337f082874820e9c8a047b11095260f2912
avmustafaev <alexander.mustafaev@gmail.com>
2022-11-26 10:09:37 +0500

    f

--- exb_test.py
+++ exb_test.py
@@ -1,8 +1,9 @@
 import modules.exbitron as exb
 
 client = exb.Client(access_key = '41be1bdef5d879f9', secret_key = '2a58e1026cbc0be5d838c0886b9275b0')
-rxd = client.get("/api/v2/peatio/account/balances/rxd")
-print(rxd)
+# rxd = client.get("/api/v2/peatio/account/balances/rxd")
+print(client.check_auth())
+# print(rxd)
 
 
 #rxd = usdt = round(float(client.get("/api/v2/peatio/account/balances/rxd")['balance']),2)
