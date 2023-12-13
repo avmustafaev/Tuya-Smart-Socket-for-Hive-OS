02784a34e27a8e0c796b481ef171a560a2c8335e
avmustafaev <alexander.mustafaev@gmail.com>
2023-12-13 13:28:37 +0500

    r

--- modules/start_hour.py
+++ modules/start_hour.py
@@ -9,7 +9,8 @@ class StartHour:
         now = datetime.now()
         current_minute = int(now.strftime("%M"))
         current_hour = int(now.strftime("%H"))
-        hour_in = current_hour in {9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21}
+        #hour_in = current_hour in {9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21}
+        hour_in = current_hour in {9}
         minute_in = current_minute >= 0 and current_minute < self.minutes_window
         responce = minute_in and hour_in
         if responce:
