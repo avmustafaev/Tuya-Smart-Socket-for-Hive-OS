ec4c148473187297ddd5623c05d7caf965a80c83
avmustafaev <alexander.mustafaev@gmail.com>
2025-07-06 23:59:29 +0500

    7

--- modules/hive_api.py
+++ modules/hive_api.py
@@ -1,11 +1,15 @@
 import json
 from time import sleep
 import requests
+from modules.telega import SendTelega
+from modules.loadenvi import Envi
 
 
 class HiveAPI:
     def __init__(self, osapi) -> None:
         self.osapi = osapi
+        self.envii = Envi()
+        self.telegramer = SendTelega(envii)
 
     def hiveos_requests_api(self, requests_part):
         url = "https://api2.hiveos.farm/api/v2/farms"
@@ -18,9 +22,11 @@ class HiveAPI:
         response_from_api = requests.get(url_full, headers=headers) 
         while response_from_api.status_code != 200:
             print(f"Error: {response_from_api.status_code}, sleep 10sec, and repeat...")
+            self.telegramer.do_telega(f"Error: {response_from_api.status_code}, sleep 10sec, and repeat...")
             sleep(10)
             response_from_api = requests.get(url_full, headers=headers)
         print("good connect to api hive os!")
+        self.telegramer.do_telega("good connect to api hive os!")
         return response_from_api.json()
 
     def hiveos_api_patch(self, wallet_id):
